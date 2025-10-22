d037cecd5537bd9590f7dd641c4c506c6b4070a7
leilaweicman <leilaweic@hotmail.com>
2025-10-22 11:12:02 +0200

    feat: implement Undo class using Singleton pattern

--- src/main/java/nivel1/Undo.java
+++ src/main/java/nivel1/Undo.java
@@ -1,17 +1,37 @@
 package nivel1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Undo {
 
     private static Undo instance;
+    private final List<String> commands = new ArrayList<>();
 
-    private Undo() {
-
-    }
+    private Undo() {}
 
     public static Undo getInstance() {
         if (instance == null) {
             instance = new Undo();
         }
         return instance;
+    }
+
+    public void addCommand(String command) {
+        commands.add(command);
+    }
+
+    public void undoLastCommand() {
+        if (hasCommands()) {
+            commands.remove(commands.size() - 1);
+        }
+    }
+
+    public List<String> getCommands() {
+        return new ArrayList<>(commands);
+    }
+
+    public boolean hasCommands() {
+        return !commands.isEmpty();
     }
 }
