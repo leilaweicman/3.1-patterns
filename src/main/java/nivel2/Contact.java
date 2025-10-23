1849cc321f87b0433a945b247fd4f92fe488e3e0
leilaweicman <leilaweic@hotmail.com>
2025-10-23 11:07:29 +0200

    feat: implement Contact entity with name, address and phone

--- src/main/java/nivel2/Contact.java
+++ src/main/java/nivel2/Contact.java
@@ -1,4 +1,21 @@
 package nivel2;
 
+import nivel2.products.Address;
+import nivel2.products.PhoneNumber;
+
 public class Contact {
+
+    private final String name;
+    private final Address address;
+    private final PhoneNumber phoneNumber;
+
+    public Contact(String name, Address address, PhoneNumber phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getInfo() {
+        return "Name: " + name + "\nAddress: " + address.getFullAddress() + "\nPhone: " + phoneNumber.getFullNumber();
+    }
 }
