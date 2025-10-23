45810545e8b9bded5f3eb1f293261a0f7aa10197
leilaweicman <leilaweic@hotmail.com>
2025-10-23 11:08:03 +0200

    feat: add Main class demonstrating Abstract Factory usage with Spain and USA contacts

--- src/main/java/nivel2/Main.java
+++ src/main/java/nivel2/Main.java
@@ -1,7 +1,29 @@
 package nivel2;
 
+import nivel2.factories.ContactFactory;
+import nivel2.factories.SpainFactory;
+import nivel2.factories.USAFactory;
+
 public class Main {
     public static void main(String[] args) {
+
+        ContactFactory spainFactory = new SpainFactory();
+        Contact spainContact = new Contact("Pedro",
+                spainFactory.createAddress("Calle Mayor 10", "Madrid"),
+                spainFactory.createPhone("600123456")
+        );
+
+        ContactFactory USAFactory = new USAFactory();
+        Contact USAContact = new Contact("Jhon",
+                USAFactory.createAddress("742 Evergreen Terrace", "Springfield"),
+                USAFactory.createPhone("2025550100")
+        );
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact(spainContact);
+        addressBook.addContact(USAContact);
+
+        System.out.println(addressBook.getAllContactsInfo());
 
     }
 }
