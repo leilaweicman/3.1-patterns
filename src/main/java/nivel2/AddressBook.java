067819e6fed84d497f5ef5444c26295fa979f5ad
leilaweicman <leilaweic@hotmail.com>
2025-10-23 11:07:44 +0200

    feat: add AddressBook class to store and display multiple contacts

--- src/main/java/nivel2/AddressBook.java
+++ src/main/java/nivel2/AddressBook.java
@@ -1,4 +1,24 @@
 package nivel2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
+    private final List<Contact> contacts = new ArrayList<>();
+
+    void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public String getAllContactsInfo() {
+        if (contacts.isEmpty()) return "No contacts in address book.";
+
+        StringBuilder result = new StringBuilder("\nAddress Book:\n");
+
+        for (Contact contact : contacts) {
+            result.append("\n").append(contact.getInfo()).append("\n");
+        }
+
+        return result.toString();
+    }
 }
